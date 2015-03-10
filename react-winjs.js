f9f9bb6ba81a15aa8eb749f7aac98465c64808bd
Adam Comella <rigdern@gmail.com>
2015-03-10 00:40:11 -0700

    Add draft of implementation of Hub

--- react-winjs.js
+++ react-winjs.js
@@ -441,8 +441,177 @@ defineControl("DatePicker");
 // TODO: FlipView
 // TODO: Flyout
 // TODO: GridLayout
-// TODO: Hub
-// TODO: HubSection
+
+// TODO: Revisit all of this diffing stuff:
+//   - Make it more efficient
+//   - It's currently hard to understand because it makes aggressive
+//     assumptions (e.g. each item has a key and each item has a winControl)
+//   - Is it correct?
+//   - Should we just sync an array with a binding list instead of computing
+//     edits based on 2 arrays and then applying them to a binding list?
+function buildIndex(array) {
+    var index = {};
+    array.forEach(function (item, i) {
+        index[item.key] = i;
+    });
+    return index;
+}
+function indexOfKey(array, key) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].key === key) {
+            return i;
+        }
+    }
+    return -1;
+}
+function diffArraysByKey(old, latest) {
+    old = old.slice(0);
+    var oldIndex = buildIndex(old);
+    var latestIndex = buildIndex(latest);
+    var edits = [];
+
+    // Handle removals
+    for (i = old.length - 1; i >= 0; i--) {
+        var item = old[i];
+        if (!latestIndex.hasOwnProperty(item.key)) {
+            edits.push({ type: "delete", index: i });
+            console.log(JSON.stringify(edits[edits.length - 1]));
+            old.splice(i, 1);
+        }
+    }
+
+    // Handle insertions and moves
+    for (i = 0; i < latest.length; i++) {
+        var item = latest[i];
+        if (!oldIndex.hasOwnProperty(item.key)) {
+            // Insertion
+            edits.push({ type: "insert", index: i, value: item });
+            console.log(JSON.stringify({ type: "insert", index: i, value: item.key }));
+            old.splice(i, 0, item);
+        } else if (old[i].key !== item.key) {
+            // Move
+            //edits.push({ type: "move", from: oldIndex[item.key], to: i });
+            //old.splice(oldIndex[item.key], 1);
+
+            var fromIndex = indexOfKey(old, item.key);
+            edits.push({ type: "move", from: fromIndex, to: i });
+            console.log(JSON.stringify(edits[edits.length - 1]));
+            old.splice(fromIndex, 1);
+            old.splice(i, 0, item);
+        }
+    }
+
+    return edits;
+}
+function applyEditsToBindingList(list, edits) {
+    edits.forEach(function (edit) {
+        if (edit.type === "delete") {
+            list.splice(edit.index, 1);
+        } else if (edit.type === "insert") {
+            list.splice(edit.index, 0, edit.value.winControl);
+        } else if (edit.type === "move") {
+            list.move(edit.from, edit.to);
+        } else {
+            throw "Unsupported edit type: " + edit.type;
+        }
+    }, this);
+}
+
+ReactWinJS.Hub = React.createClass({
+    shouldComponentUpdate: function () {
+        return false;
+    },
+    componentDidMount: function () {
+        this.winControl = new WinJS.UI.Hub(
+            this.getDOMNode(),
+            selectKeys(ControlApis.Hub.properties, this.props)
+        );
+        ControlApis.Hub.events.forEach(function (eventName) {
+            if (this.props.hasOwnProperty(eventName)) {
+                this.winControl[eventName.toLowerCase()] = this.props[eventName];
+            }
+        }, this);
+        this.hubSectionRoots = [];
+        this.hubSectionRootsMap = {};
+        React.Children.forEach(this.props.children, function (component) {
+            if (component) {
+                var root = new HubSectionRoot(component);
+                this.hubSectionRoots.push(root);
+                this.hubSectionRootsMap[component.key] = root;
+                this.winControl.sections.push(root.winControl);
+            }
+        }, this);
+    },
+    componentWillUnmount: function () {
+        this.winControl.dispose && this.winControl.dispose();
+    },
+    componentWillReceiveProps: function (nextProps) {
+        ControlApis.Hub.properties.forEach(function (propName) {
+            if (nextProps.hasOwnProperty(propName) && this.winControl[propName] !== nextProps[propName]) {
+                this.winControl[propName] = nextProps[propName];
+            }
+        }, this);
+        ControlApis.Hub.events.forEach(function (eventName) {
+            var lowerEventName = eventName.toLowerCase();
+            if (nextProps.hasOwnProperty(eventName) && this.winControl[lowerEventName] !== nextProps[eventName]) {
+                this.winControl[lowerEventName] = nextProps[eventName];
+            }
+        }, this);
+
+        var newHubSectionRoots = [];
+        var newHubSectionRootsMap = {};
+        React.Children.forEach(nextProps.children, function (component) {
+            if (component) {
+                var root = this.hubSectionRootsMap[component.key];
+                if (root) {
+                    root.update(component);
+                } else {
+                    root = new HubSectionRoot(component);
+                }
+                newHubSectionRoots.push(root);
+                newHubSectionRootsMap[component.key] = root;
+            }
+        }, this);
+
+        applyEditsToBindingList(
+            this.winControl.sections,
+            diffArraysByKey(this.hubSectionRoots, newHubSectionRoots)
+        );
+        
+        this.hubSectionRoots = newHubSectionRoots;
+        this.hubSectionRootsMap = newHubSectionRootsMap;
+    },
+    render: function() {
+        return React.DOM.div();
+    }
+});
+var HubSectionRoot = function (component) {
+    this.winControl = new WinJS.UI.HubSection(
+        null,
+        selectKeys(ControlApis.HubSection.properties, component.props)
+    );
+    this.key = component.key;
+
+    component.winControl = this.winControl;
+    React.render(component, this.winControl.contentElement);
+};
+HubSectionRoot.prototype.update = function (component) {
+    var nextProps = component.props;
+    ControlApis.HubSection.properties.forEach(function (propName) {
+        if (nextProps.hasOwnProperty(propName) && this.winControl[propName] !== nextProps[propName]) {
+            this.winControl[propName] = nextProps[propName];
+        }
+    }, this);
+
+    component.winControl = this.winControl;
+    React.render(component, this.winControl.contentElement);
+};
+ReactWinJS.HubSection = React.createClass({
+    render: function() {
+        return this.props.children;
+    }
+});
+
 defineControl("ItemContainer", {
     mounts: {
         children: function (winControl) {
