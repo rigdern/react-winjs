74988a78599787e4d66f796ed3609cf053f3b073
Adam Comella <rigdern@gmail.com>
2015-04-13 23:48:00 -0700

    ItemContainer: tabs to spaces

--- examples/showcase/examples/ItemContainer.jsx
+++ examples/showcase/examples/ItemContainer.jsx
@@ -5,8 +5,8 @@ var ReactWinJS = require('react-winjs');
 
 module.exports = React.createClass({
     handleSelectionChanged: function (eventObject) {
-    	var itemContainer = eventObject.currentTarget.winControl;
-    	this.setState({ selected: itemContainer.selected });
+        var itemContainer = eventObject.currentTarget.winControl;
+        this.setState({ selected: itemContainer.selected });
     },
     getInitialState: function () {
         return {
@@ -15,17 +15,17 @@ module.exports = React.createClass({
     },
     render: function () {
         return (
-			<ReactWinJS.ItemContainer
-				style={{margin: 5, height: 80, width: 252}}
-				tapBehavior="toggleSelect"
-				selected={this.state.selected}
-				onSelectionChanged={this.handleSelectionChanged}>
-				<div style={{padding: 5}}>
-					<h4>Marvelous Mint</h4>
-					<h6>Gelato</h6>
-					Selected: {this.state.selected.toString()}
-				</div>
-			</ReactWinJS.ItemContainer>
+            <ReactWinJS.ItemContainer
+                style={{margin: 5, height: 80, width: 252}}
+                tapBehavior="toggleSelect"
+                selected={this.state.selected}
+                onSelectionChanged={this.handleSelectionChanged}>
+                <div style={{padding: 5}}>
+                    <h4>Marvelous Mint</h4>
+                    <h6>Gelato</h6>
+                    Selected: {this.state.selected.toString()}
+                </div>
+            </ReactWinJS.ItemContainer>
         );
     }
 });
